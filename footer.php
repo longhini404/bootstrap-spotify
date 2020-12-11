ad46145c6e3ab42ff90ed4702d69cc1eb7c75dd2
Guilherme Longhini <guilherme.longhini@gmail.com>
2020-12-11 19:10:53 -0300

    Header & Footer
    Include

---
+++ footer.php
@@ -0,0 +1,123 @@
+<footer class="page-footer bg-black color-white" style="padding-top: 3rem">
+  <div class="container">
+    <div class="row">
+      <div class="col-2 mt-4">
+        <a href="https://www.spotify.com/br/">
+          <img src="assets/brand/spotify-logo-branca.png" width="140" height="40" class="d-inline-block align-top mr-auto" alt="spotify Logo Branca" />
+        </a>
+      </div>
+
+      <div class="col-2">
+        <p class="mt-4 mb-4 text-muted" style="font-size: small;"><strong>EMPRESA</strong></p>
+        <ul class="list-unstyled">
+          <li class="pb-3">
+            <a class="text-white" href="https://www.spotify.com/br/about-us/contact/">Sobre</a>
+          </li>
+          <li class="pb-3">
+            <a class="text-white" href="https://www.spotifyjobs.com/?_ga=2.241578114.1032510707.1607106675-762130387.1599533115">Empregos</a>
+          </li>
+          <li class="pb-3">
+            <a class="text-white" href="https://newsroom.spotify.com/?_ga=2.241578114.1032510707.1607106675-762130387.1599533115">For the Record</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-2">
+        <p class="mt-4 mb-4 text-muted" style="font-size: small;"><strong>COMUNIDADES</strong></p>
+        <ul class="list-unstyled">
+          <li class="pb-3">
+            <a class="text-white" href="https://artists.spotify.com/?_ga=2.136255665.1032510707.1607106675-762130387.1599533115">Para Artistas</a>
+          </li>
+          <li class="pb-3">
+            <a class="text-white" href="https://developer.spotify.com/?_ga=2.136255665.1032510707.1607106675-762130387.1599533115">Desenvolvedores</a>
+          </li>
+          <li class="pb-3">
+            <a class="text-white" href="https://ads.spotify.com/?_ga=2.136255665.1032510707.1607106675-762130387.1599533115">Publicidade</a>
+          </li>
+          <li class="pb-3">
+            <a class="text-white" href="https://investors.spotify.com/?_ga=2.136255665.1032510707.1607106675-762130387.1599533115">Investidores</a>
+          </li>
+          <li class="pb-3">
+            <a class="text-white" href="https://spotifyforvendors.com/">Fornecedores</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-2">
+        <p class="mt-4 mb-4 text-muted" style="font-size: small;"><strong>LINKS ÚTEIS</strong></p>
+        <ul class="list-unstyled">
+          <li class="pb-3">
+            <a class="text-white" href="https://support.spotify.com/br/?_ga=2.237360256.1032510707.1607106675-762130387.1599533115">Suporte</a>
+          </li>
+          <li class="pb-3">
+            <a class="text-white" href="https://open.spotify.com/?_ga=2.145176756.1032510707.1607106675-762130387.1599533115">Player da Web</a>
+          </li>
+          <li class="pb-3">
+            <a class="text-white" href="https://www.spotify.com/br/download/windows/b/">Aplicativo móvel grátis</a>
+          </li>
+          <li class="pb-3">
+            <a class="text-white" href="https://open.spotify.com/genre/2020-page?locale=pt-BR">Retrospectiva 2020</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-4 pt-4" style="text-align: right;">
+        <ul class="list-unstyled list-inline">
+          <li class="list-inline-item">
+            <a class="btn-floating btn-fb text-white px-2" href="https://instagram.com/spotify">
+              <i class="fab fa-instagram fa-2x social-media-icon"> </i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="btn-floating btn-tw text-white px-2" href="https://twitter.com/spotify">
+              <i class="fab fa-twitter fa-2x social-media-icon"> </i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="btn-floating btn-gplus text-white px-2" href="https://www.facebook.com/Spotify">
+              <i class="fab fa-facebook fa-2x social-media-icon"> </i>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <ul class="list-unstyled list-inline content" style="padding-top: 6rem; font-size: small;">
+
+        <li class="list-inline-item">
+          <a class="text-muted" href="https://www.spotify.com/br/legal/">
+            Legal
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a class="text-muted" href="https://www.spotify.com/br/privacy/">
+            Centro de Privacidade
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a class="text-muted" href="https://www.spotify.com/br/legal/privacy-policy/">
+            Política de privacidade
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a class="text-muted" href="https://www.spotify.com/br/legal/cookies-policy/">
+            Cookies
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a class="text-muted" href="https://www.spotify.com/br/legal/privacy-policy/#s3">
+            Sobre anúncios
+          </a>
+        </li>
+        <div style="text-align: right;">
+          <li class="list-inline-item">
+            <p class="text-muted">
+              Brasil
+              <img src="assets/img/Brasil.jpg" width="20" height="20" class="mx-1" alt="Bandeira do Brasil" />
+            </p>
+            <p class="text-muted">© 2020 Spotify AB</p>
+          </li>
+        </div>
+      </ul>
+    </div>
+  </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,39 @@
+<header class="header-sticky header-dark">
+  <nav class="navbar navbar-expand-lg navbar-light bg-black">
+    <div class="container">
+      <a href="https://www.spotify.com/br/">
+        <img src="assets/brand/spotify-logo-branca.png" width="132" height="40" class="d-inline-block align-top mr-auto" alt="spotify Logo Branca" />
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link text-white m-3" href="https://www.spotify.com/br/premium/">Premium</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white m-3" href="https://support.spotify.com/br/?_ga=2.204242064.1032510707.1607106675-762130387.1599533115">Suporte</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white m-3" href="https://www.spotify.com/br/download/windows/b/">Baixar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white m-3">|</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src="assets/img/profile-picture.jpg" width="40" height="40" class="rounded-circle m-2" alt="Profile Picture" />
+              Perfil
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="https://www.spotify.com/br/account/overview/">Conta</a>
+              <a class="dropdown-item text-muted" href="https://www.spotify.com/br/logout/">Sair</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
