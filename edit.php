6f80206922be7aebe1a800cadbb3afcb205f3cfe
Guilherme Longhini <guilherme.longhini@gmail.com>
2020-12-11 19:10:17 -0300

    Form PHP - Lógica Data de Nascimento

---
+++ edit.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+
+// var_dump($_REQUEST);
+
+// die();
+
+$day = $_POST['day'];
+$month = $_POST['month'];
+$year = $_POST['year'];
+
+try {
+    $dataNascimento = "$year-$month-$day";
+    $date = new DateTime($dataNascimento);
+    $intervalo = $date->diff(new DateTime(date('Y-m-d')));
+    echo $intervalo->format('%Y anos');
+
+    if ($intervalo->format('%Y') < 14) {
+        $_SESSION['mensagem'] = "Menor de idade";
+    } else {
+        $_SESSION['mensagem'] = "Perfil salvo";
+    }
+} catch (Exception $e) {
+    $_SESSION['mensagem'] = "Idade Inválida";
+}
+
+header("Location: editar-perfil.php");
