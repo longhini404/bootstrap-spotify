c3a975cc8f0e00802e18aa67a9315d935835d17c
Guilherme Longhini <guilherme.longhini@gmail.com>
2020-12-12 03:21:42 -0300

    Não deixar ser cadastrado idade inferior a 14 anos

--- edit.php
+++ edit.php
@@ -16,6 +16,9 @@ try {
         $_SESSION['mensagem_erro'] = "Menor de idade";
     } else {
         $_SESSION['mensagem_sucesso'] = "Perfil salvo";
+        $_SESSION['day'] = $_POST['day'];
+        $_SESSION['month'] = $_POST['month'];
+        $_SESSION['year'] = $_POST['year'];
     }
 } catch (Exception $e) {
     $_SESSION['mensagem_erro'] = "Data de nascimento inválida";
@@ -23,9 +26,7 @@ try {
 
 $_SESSION['email'] = $_POST['email'];
 $_SESSION['sex'] = $_POST['sex'];
-$_SESSION['day'] = $_POST['day'];
-$_SESSION['month'] = $_POST['month'];
-$_SESSION['year'] = $_POST['year'];
 $_SESSION['country'] = $_POST['country'];
+$_SESSION['checkbox'] = $_POST['checkbox'];
 
 header("Location: editar-perfil.php");
