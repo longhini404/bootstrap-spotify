4176a2920f35b104c82faac7bdd2963342477e02
Guilherme Longhini <guilherme.longhini@gmail.com>
2020-12-17 17:29:01 -0300

    Script src Bootstrap JS

--- public/index.php
+++ public/index.php
@@ -222,7 +222,7 @@
   <?php include __DIR__ . '/../layouts/footer.php' ?>
   <!-- / Footer -->
 
-  <script src="/assets/library/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
   <script src="https://kit.fontawesome.com/446e1edcd2.js" crossorigin="anonymous"></script>
 
 </body>
